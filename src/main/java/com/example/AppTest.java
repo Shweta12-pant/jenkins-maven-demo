a08feffec0712fae835742ed4c86feb0d2fd68ed
Shweta Pant <shwetapant1037@gmail.com>
2026-09-19 23:14:02 +0530

    Update AppTest.java

--- src/main/java/com/example/AppTest.java
+++ src/main/java/com/example/AppTest.java
@@ -2,7 +2,10 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class AppTest {
 
